5455fd562511242ffa788484ff1cc50da7624448
nurulsuhadayusri <113415720+nurulsuhadayusri@users.noreply.github.com>
2022-09-14 10:33:09 +0800

    Update second.py

--- second.py
+++ second.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.header("My first Streamlit App")
+st.header("My Second Streamlit App")
 
 st.write('Before you continue, please read the [terms and conditions](https://www.gnu.org/licenses/gpl-3.0.en.html)')
 show = st.checkbox('I agree the terms and conditions')
