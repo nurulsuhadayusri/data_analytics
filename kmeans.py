8694a8bba2b3eb9eb14d36e1df40857d2b71950f
nurulsuhadayusri <113415720+nurulsuhadayusri@users.noreply.github.com>
2022-09-14 13:50:28 +0800

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,8 +1,17 @@
-from sklearn.cluster import KMeans
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+kmeans=KMeans(n_clusters=5,init='k-means++',random_state=0)
+y_kmeans=kmeans.fit_predict(data)
 
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+#plotting the the clusters
+fig,ax = plt.subplots(figsize=(14,6))
+ax.scatter(data[y_kmeans==0,0],data[y_kmeans==0,1],s=100,c='red',label='Cluster 1')
+ax.scatter(data[y_kmeans==1,0],data[y_kmeans==1,1],s=100,c='blue',label='Cluster 2')
+ax.scatter(data[y_kmeans==2,0],data[y_kmeans==2,1],s=100,c='green',label='Cluster 3')
+ax.scatter(data[y_kmeans==3,0],data[y_kmeans==3,1],s=100,c='cyan',label='Cluster 4')
+ax.scatter(data[y_kmeans==4,0],data[y_kmeans==4,1],s=100,c='magenta',label='Cluster 5')
+
+ax.scatter(kmeans.cluster_centers_[:,0],kmeans.cluster_centers_[:,1],s=400,c='yellow',label='Centroid')
+plt.title('Cluster Segmentation of Customers')
+plt.xlabel('Annual Income(K$)')
+plt.ylabel('Spending Score(1-100)')
+plt.legend()
+plt.show()
